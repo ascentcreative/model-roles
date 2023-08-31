c3c5ad325e3786f29a0ee0aea109d73454dc126e
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-08-31 22:16:10 +0100

    Users relation and grant function

--- src/Traits/HasRoleUsers.php
+++ src/Traits/HasRoleUsers.php
@@ -4,9 +4,16 @@ namespace AscentCreative\ModelRoles\Traits;
 
 use AscentCreative\ModelRoles\Models\ModelUserRole;
 
+use Staudenmeir\EloquentHasManyDeep\HasRelationships; 
+use Staudenmeir\EloquentHasManyDeep\HasManyDeep;
+
+use App\Models\User;
+
 // Trait to apply to the target Model
 // Gives access to shortcut methods to see which users have roles etc
 trait HasRoleUsers {
+
+    use HasRelationships;
 
     public function hasRoleUsers($roles) {
 
@@ -31,8 +38,42 @@ trait HasRoleUsers {
                     ->whereIn('role', $roles);
     }
 
+    public function users() {
+        return $this->hasManyDeep(
+            User::class,
+            [ModelUserRole::class],
+            [
+                ['model_type', 'model_id'],
+                'id'
+            ],
+            [
+                null,
+                'user_id'
+            ]
+
+        );
+    }
+
     public function modelRoles() {
         return $this->morphMany(ModelUserRole::class, 'model');
+    }
+
+    public function grantUserRole($user, $role) {
+
+        if(is_numeric($user)) {
+            $user_id = $user;
+        } else {
+            $user_id = $user->id;
+        }
+
+        $mr = ModelUserRole::updateOrCreate([
+            'user_id'=>$user_id,
+            'model_type' => get_class($this),
+            'model_id' => $this->id,
+        ], [
+            'role'=>$role,
+        ]);
+
     }
 
 
